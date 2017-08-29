b4fa861b3135ebf8fe4ad5711ac3982bc2b15808
rogeriomil <rogerio.nunesp@gmail.com>
2017-08-28 22:51:26 -0300

    removind re-bulma pckg

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import CategoryList from '../containers/category-list';
 import JokeContainer from '../containers/joke-container';
 import Header from './Header';
-import { Columns, Column } from 're-bulma';
+
 
 export default class App extends Component {
   render() {
